7b3b5ca05011de1524df03158bb11ad3b2fcc700
David Fernandes <122941959+davidfndss@users.noreply.github.com>
2023-11-27 12:01:33 -0300

    learning handlebars

--- public/script/index.js
+++ public/script/index.js
@@ -3,19 +3,24 @@ const app = express()
 require('dotenv').config()
 const handlebars = require("express-handlebars")
 const path = require("path")
-const db = require("./db.js")
 const Post = require("./models/Post.js")
 
 //Config
     //Template engine
-    const handlebarsInstance = handlebars.create({ defaultLayout: "main" });
+    const handlebarsInstance = handlebars.create({ defaultLayout: "main",
+    runtimeOptions: {
+        allowProtoPropertiesByDefault: true,
+        allowProtoMethodsByDefault: true
+    } });
     app.engine("handlebars", handlebarsInstance.engine)
     app.set("view engine","handlebars")
     // Configuração para servir arquivos estáticos
     app.use(express.static(path.join(__dirname,"../")))
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname,"../","../home.html"))
+    Post.findAll().then((posts)=>{
+        res.render("home",{posts:posts})
+    }) 
 })
 
 const port = process.env.PORT || 7777
